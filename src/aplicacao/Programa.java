7f4bc421e6a49d67ae5913237d9c78d10f7a4919
oliveiraroberto702 <robertvascol702@gmail.com>
2026-07-27 13:25:46 -0300

    implementacao incial predicate clássica

---
+++ src/aplicacao/Programa.java
@@ -0,0 +1,28 @@
+package aplicacao;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import entidades.Produto;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		List<Produto> lista = new ArrayList<>();
+
+		lista.add(new Produto("TV", 900.00));
+		lista.add(new Produto("Mouse", 50.00));
+		lista.add(new Produto("Notebook", 1200.00));
+		lista.add(new Produto("Tablet", 450.00));
+		lista.add(new Produto("HD Case", 80.90));
+
+		lista.removeIf(p -> p.getPreco() > 100.00);
+
+		for (Produto p : lista) {
+			System.out.println(p);
+		}
+	}
+
+}
